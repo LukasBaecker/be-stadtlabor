9cc448812cd018a056a2bf5fb9c4f6a8d5093bd5
Brian Pondi <pondibrian@gmail.com>
2021-12-01 21:05:06 +0100

    Username required fields

--- urbangarden/users/models.py
+++ urbangarden/users/models.py
@@ -9,11 +9,11 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     user_id = models.AutoField(primary_key=True, editable=False)
-    password = models.CharField(max_length=50)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255, unique=True, null=False)
-    phone = models.CharField(max_length=20)
+    phone = models.CharField(max_length=255)
     date_created = models.DateTimeField(auto_now_add=True)
     is_logged_in = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
@@ -21,10 +21,11 @@ class User(AbstractUser):
     garden = models.ManyToManyField('gardens.Garden')
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["username",]
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+
     
 class GardenMembership(models.Model):
     ROLE_CHOICE = (
