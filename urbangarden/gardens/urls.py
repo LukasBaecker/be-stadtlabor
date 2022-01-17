a1bbb2d5298ec5253bcd79dca94739e21fbcf779
Nivi106 <93142842+Nivi106@users.noreply.github.com>
2022-01-17 18:55:44 +0100

    geocoding changed version

--- urbangarden/gardens/urls.py
+++ urbangarden/gardens/urls.py
@@ -1,17 +1,15 @@
 from django.conf.urls import url
-from .views import GardenView, GardenDetailView, GardenDetailViewPost, ResourceView, ResourceDetailView, ResourceDetailViewPost, GetCoordinatesFromAddress
+from .views import GardenView, GardenDetailView, GardenDetailViewPost, ResourceView, ResourceDetailView, ResourceDetailViewPost
 from django.urls import path, include
 
 from django.urls import path
 
 urlpatterns = [
-        #Gardens
-        path("coordinates", # the rest will be in views: ?address=QUERY_ADDRESS
-          GetCoordinatesFromAddress.as_view(), name="get-coordinates"),
+       
         path('all', GardenView.as_view()),
         path('<pk>', GardenDetailView.as_view()),
         path('', GardenDetailViewPost.as_view()),
-       
+]     
 
 from rest_framework import routers
 
